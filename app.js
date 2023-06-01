48c5b4e78ea3f65e39e099d0c845b23decfc4783
DevDoBim <zmdkdna2@naver.com>
2023-06-01 16:09:27 +0900

    search success & css modify

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-let btn = document.querySelector("#search-button");
-btn.addEventListener("click", function () {
-    getData();
-    alert();
-});
-
 let resData;
 
+function filter() {
+    let search = document.getElementById("searchInput").value.toLowerCase();
+    let cardInner = document.getElementsByClassName("col");
+
+    for (let i = 0; i < cardInner.length; i++) {
+      //city = listInner[i].getElementsByClassName("city");
+      cardTitle = cardInner[i].getElementsByClassName("cardTitle");
+      if (//city[0].innerHTML.toLowerCase().indexOf(search) != -1 ||
+      cardTitle[0].innerHTML.toLowerCase().indexOf(search) != -1
+      ) {
+        cardInner[i].style.display = "flex"
+      } else {
+        cardInner[i].style.display = "none"
+      }
+    }
+  }
+
+//TMDB 오픈 API -> Movie Top Rated
 function getData() {
     const options = {
         method: 'GET',
@@ -21,33 +33,37 @@ function getData() {
         .catch(err => console.error(err));
 }
 
- function renderData(data) {
+function renderData(data) {
     // let data = await getData();
-    let html = '';
-    //console.log("test : " + data);
-    resData = data['results'];
-    //console.log(resData);
+    let html = ''; // html 변수 선언 및 초기화
+    // console.log("test : " + data);
+    resData = data['results']; // resData 변수에는 불러온 데이터를 배열형식으로 저장한다.
+    // console.log(resData);
 
-    for(let i = 0; i < resData.length; i++ ){
-        //console.log(resData[i]);
+    //함수 renderData는 fetch를 통해 받아온 데이터를 매개변수로 받는다.
+    for (let i = 0; i < resData.length; i++) {
+        // console.log(resData[i]);
         let htmlSegment = `<div class="col">
                                 <div class="card h-100">
                                     <img src="https://image.tmdb.org/t/p/w500${resData[i].poster_path}"
                                         class="card-img-top">
-                                    <div class="card-body">
-                                        <h5 class="card-title">${resData[i].title}</h5>
-                                        <p class="card-text">${resData[i].overview}</p>
+                                    <div class="cardBody">
+                                        <h5 class="cardTitle">${resData[i].title}</h5>
+                                        <p class="cardText">${resData[i].overview}</p>
+                                        <p>Rating : ${resData[i].popularity}</p>
                                     </div>
                                 </div>
                             </div>`;
         html += htmlSegment;
     }
-    
+
     let appendMycards = document.querySelector('#mycards');
     appendMycards.innerHTML = html;
 }
 
 getData();
+
+
 
 // data.forEach(data => {
     //     console.log("작동");
